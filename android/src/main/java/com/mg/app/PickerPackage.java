8f9f2e5f7acd3e9ed5c5f2aa46b79f6fed661966
Kevin Morales <32549527+kmorales13@users.noreply.github.com>
2017-12-20 14:24:51 -0600

    Update PickerPackage.java

--- android/src/main/java/com/mg/app/PickerPackage.java
+++ android/src/main/java/com/mg/app/PickerPackage.java
@@ -6,17 +6,23 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public abstract class PickerPackage implements ReactPackage {
-
+public class PickerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new PickerModule(reactContext));
-
-        return modules;
+      return Arrays.<NativeModule>asList(new PickerModule(reactContext));
+    }
+ 
+    // Deprecated in React Native 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
+    }
+ 
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+      return Collections.emptyList();
     }
 }
